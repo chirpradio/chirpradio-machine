e549604969f6ae36d89cb8a35e6078180be43959
Trevor Borg <trevor.borg@gmail.com>
2012-12-05 15:03:46 -0600

    updated appengine path

--- chirp/common/chirpradio.py
+++ chirp/common/chirpradio.py
@@ -40,6 +40,4 @@ def connect(host=None):
     if host is None:
         host = conf.CHIRPRADIO_HOST
     remote_api_stub.ConfigureRemoteDatastore(
-        "chirpradio-hrd", "/remote_api", _auth_func, host)
-
-
+        "chirpradio-hrd", "/_ah/remote_api", _auth_func, host)
