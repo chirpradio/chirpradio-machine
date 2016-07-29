6d56b49aa558888c1bc01574b7e9de6143a39706
Feihong Hsu <feihong.hsu@gmail.com>
2016-07-29 14:22:40 -0500

    Add new module, printing

---
+++ chirp/common/printing.py
@@ -0,0 +1,30 @@
+import contextlib
+
+
+class CustomPrint(object):
+    """
+    This class defines a callable object that works similarly to the print()
+    function. However, it provides a context manager that allows you to specify
+    a different write function that can be used to send messages to somewhere
+    other than standard output (for example, a web interface).
+
+    """
+
+    def __init__(self):
+        self.write = self.default_write
+
+    def __call__(self, message, **kwargs):
+        self.write(message, **kwargs)
+
+    def default_write(self, message, **kwargs):
+        if message:
+            print(message)
+
+    @contextlib.contextmanager
+    def use_write_function(self, func):
+        self.write = func
+        yield
+        self.write = self.default_write
+
+
+cprint = CustomPrint()
