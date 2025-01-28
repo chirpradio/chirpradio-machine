b400fb91fc1abde5f731b5fdb3e1ed6fcbceb154
Benjamin Lowry <blowry@uchicago.edu>
2025-01-27 18:26:36 -0600

    testsuit: don't run stream tests

--- testsuite.sh
+++ testsuite.sh
@@ -4,8 +4,9 @@
 #to see error messages delete "2> /dev/null"
 
 for filename in **/library/*_test.py; do
-    path=${filename//\//.}
-    command=${path/.py/}
-    python3 -m $command 2> /dev/null || echo failed on $filename
-    
+    if [[ $filename != *"stream"* ]]; then
+	    path=${filename//\//.}
+	    command=${path/.py/}
+	    python3 -m $command 2> /dev/null || echo failed on $filename
+    fi
 done
