dd07bd92324727513ee093666ed63a16c80a1c9a
Mya Nguyen <ngnm61@uchicago.edu>
2025-01-27 18:38:09 -0600

    fix echo

--- testsuite.sh
+++ testsuite.sh
@@ -3,9 +3,9 @@
 #to run tests of speciifc directory, replace "**/*/*_test.py" with "**/directory/*_test.py"
 #to see error messages delete "2> /dev/null"
 
-for filename in **/*/*_test.py; do
+for filename in **/library/*_test.py; do
     path=${filename//\//.}
     command=${path/.py/}
-    python3 -m $command 2> /dev/null && echo failed on $filename
+    python3 -m $command 2> /dev/null || echo failed on $filename
     
 done
